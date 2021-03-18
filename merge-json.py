3f37fecf2e91dcd57d7f993545a9b5b47a13e931
Shridhar <36041359+shridharMe@users.noreply.github.com>
2021-03-18 17:43:20 +0000

    Update merge-json.py

--- merge-json.py
+++ merge-json.py
@@ -1,10 +1,10 @@
 from pprint import pprint
 from jsonmerge import merge
 
-f1 = open("project/target/bom.json", "r")
+f1 = open("project1/target/bom.json", "r")
 
 
-f2 = open("project/target/bom.json", "r")
+f2 = open("project2/target/bom.json", "r")
 
 result = merge(f1.read(), f2.read())
 
